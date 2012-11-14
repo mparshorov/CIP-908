05dead5ad51e0fa61a6fae253d3acfd28d6c092d
Martin Parshorov <martin.parshorov@bitmix.co.uk>
2012-11-14 15:53:40 +0200

    Initial structure of project

---
+++ test/uk/co/newsint/cip/utilities/html/DataCleaningHTMLTest.java
@@ -0,0 +1,22 @@
+package uk.co.newsint.cip.utilities.html;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * JUnit tests.
+ * 
+ * @author BitMix
+ *
+ */
+public class DataCleaningHTMLTest
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}

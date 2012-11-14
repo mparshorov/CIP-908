15a67eb7cdaf2e326347e946c78342e88efc8a19
Martin Parshorov <martin.parshorov@bitmix.co.uk>
2012-11-14 16:20:05 +0200

    Added initial test

--- test/uk/co/newsint/cip/utilities/html/DataCleaningHTMLTest.java
+++ test/uk/co/newsint/cip/utilities/html/DataCleaningHTMLTest.java
@@ -12,11 +12,34 @@ import org.junit.Test;
  */
 public class DataCleaningHTMLTest
 {
+    /**
+     * Instance of LibDataCleaningHTML to parse Strings later in the tests.
+     */
+    LibDataCleaningHTML testClean = new LibDataCleaningHTML();
 
+    /**
+     * Initial test for converting encoded URL to non-encoded URL.
+     */
     @Test
     public void test()
     {
-        fail("Not yet implemented");
+        assertString("http://www.permadi.com/tutorial/urlEncoding/example.html?var=This+is+a+simple+%26+short+test.",
+                "http://www.permadi.com/tutorial/urlEncoding/example.html?var=This is a simple & short test.");
+    }
+
+    /**
+     * Initial assert method.
+     * 
+     * @param input
+     * @param expectedOutput
+     */
+    private void assertString(String input, String expectedOutput)
+    {
+        String actualClean = testClean.parse(input);
+        assertNotNull("Invalid String!", input);
+        assertEquals("Fail", expectedOutput, actualClean);
+        
+        
     }
 
 }

9a0ed912c540dc509bf16e35451df777b81034e2
Gpetkov <petkov_hcb@abv.bg>
2012-11-15 16:42:13 +0200

    Added new packages

---
+++ src/uk/co/newsint/cip/utilities/htmldataclean/HTMLDataCleaner.java
@@ -0,0 +1,13 @@
+package uk.co.newsint.cip.utilities.htmldataclean;
+
+/**
+ * Class for a library to be implemented.
+ * 
+ * @author BitMix
+ *
+ */
+
+public class HTMLDataCleaner
+{
+    //TODO: class body.
+}

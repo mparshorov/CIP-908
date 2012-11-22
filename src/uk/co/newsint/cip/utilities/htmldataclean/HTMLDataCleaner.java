7322fd16dd7cf4d0336bd9492a683462e5b6419c
Gpetkov <petkov_hcb@abv.bg>
2012-11-22 12:15:08 +0200

    Added clean method for the html

--- src/uk/co/newsint/cip/utilities/htmldataclean/HTMLDataCleaner.java
+++ src/uk/co/newsint/cip/utilities/htmldataclean/HTMLDataCleaner.java
@@ -3,6 +3,8 @@ package uk.co.newsint.cip.utilities.htmldataclean;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.net.URLCodec;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -20,42 +22,46 @@ public class HTMLDataCleaner
      */
     private static final String REGEXP_REMOVE_NONALPHA = "[^a-zA-Z]";
     /**
-     * This pattern's using the REGEX_REMOVE_NONALPHA
+     * This is the text after the clean process
      */
-    private static Pattern NONALPHA_PATTERN_CLEANER = Pattern.compile(HTMLDataCleaner.REGEXP_REMOVE_NONALPHA);
+    private String cleanedText;
+
+    /**
+     * Get the cleaned text
+     */
+    public String getCleanedText()
+    {
+        return this.cleanedText;
+    }
 
     /**
      * This method is used for cleaning the input data from html tags and url encoding
      */
-    public static String clean(String inputText)
+    public void clean(String inputText)
     {
+        // The text after cleaning
         String cleanedText = null;
 
-        return cleanedText;
-    }
+        // apache commons url codec for decoding the url's in the text
+        URLCodec urlDecoder = new URLCodec();
 
-    /**
-     * This method is used for cleaning the input data from html tags and url encoding and replacing with character
-     * @throws IOException 
-     */
-    public static String cleanAndReplace(String inputText) throws IOException
-    {
-        String cleanedText = null;
-        Document doc = Jsoup.connect("http://www.linux.org/page/view/join-linux-org").timeout(5000).get();
-        // here i can call the clean method
-        // here has to be the replace implementation
-        cleanedText = Jsoup.parse(inputText).text();
-        return cleanedText;
-    }
+        // Clean url decoding
+        try
+        {
+            cleanedText = urlDecoder.decode(inputText);
+        }
+        catch (DecoderException e)
+        {
+            e.printStackTrace();
+        }
 
-    /**
-     * This method is used for cleaning the input data from html tags and url encoding and nonalpha characters(inc spaces)
-     */
-    public static String removeNonAlphaCharacters(String inputText)
-    {
-        String cleanedText = null;
-        // here i can call the clean method
-        cleanedText = inputText.replaceAll(HTMLDataCleaner.REGEXP_REMOVE_NONALPHA, "#");
-        return cleanedText;
+        // Remove html tags from the text
+        cleanedText = Jsoup.parse(cleanedText).text();
+
+        // Remove nonalpha characters
+        cleanedText = cleanedText.replaceAll(REGEXP_REMOVE_NONALPHA, "");
+
+        this.cleanedText = cleanedText;
+        //return cleanedText;
     }
 }
